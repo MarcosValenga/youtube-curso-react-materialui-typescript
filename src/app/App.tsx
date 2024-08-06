9f7d7d5d8b23d6c78e4424b17ac5367bdfd83482
Marcos Valenga <134209298+MarcosValenga@users.noreply.github.com>
2024-08-06 15:55:51 -0400

    feat: Drawer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,14 @@
 import { AppRoutes } from "./routes"
 import { AppThemeProvider } from "./core/contexts";
+import { MenuLateral } from "./core/components";
 
 
 export const App = () => {
   return (
     <AppThemeProvider>
-      <AppRoutes />
+      <MenuLateral>
+        <AppRoutes />
+      </MenuLateral>
     </AppThemeProvider>
   );
 }
